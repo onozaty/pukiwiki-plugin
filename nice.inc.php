dc5dafa99f0ca48e1df0aa994d342d4bb0192f6b
onozaty <onozaty@gmail.com>
2021-05-16 18:38:41 +0900

    [いいねプラグイン] 履歴を保存する処理を実装

--- nice.inc.php
+++ nice.inc.php
@@ -4,13 +4,15 @@
 // This plugin is based on vote.inc.php.
 // License: GPL v2 or (at your option) any later version
 
-define('PLUGIN_NICE_LABEL', "&#x1f44d いいね！");
+define('PLUGIN_NICE_LABEL', "&#x1f44d いいね！");                    # ラベル(ボタンにラベル＋数が表示)
+define('PLUGIN_NICE_NOTIMESTAMP', true);                             # タイムスタンプを更新しない
+define('PLUGIN_NICE_HISTORY_CACHE', CACHE_DIR . 'nice_history.dat'); # 履歴の保存先
+define('PLUGIN_NICE_HISTORY_PERIOD_DAYS', 30);                       # 履歴の保存期間
 
 function plugin_nice_action()
 {
-    global $vars, $script, $cols,$rows;
+    global $vars, $script, $cols, $rows;
     global $_title_collided, $_msg_collided, $_title_updated;
-    global $_nice_plugin_nices;
 
     if (PKWK_READONLY) die_message('PKWK_READONLY prohibits editing');
 
@@ -21,12 +23,13 @@ function plugin_nice_action()
     $matches = array();
     foreach($postdata_old as $line) {
 
+        # "#nice(1)" みたいな形式
         if (! preg_match('/^#nice\((\d*)\)(.*)?$/i', $line, $matches) ||
             $nice_no++ != $vars['nice_no']) {
             $postdata .= $line;
             continue;
         }
-        $cnt = isset($matches[1]) ? $matches[1] : 0;
+        $cnt   = isset($matches[1]) ? $matches[1] : 0;
         $lefts = isset($matches[2]) ? $matches[2] : '';
 
         $cnt++;
@@ -54,7 +57,13 @@ $_msg_collided
 
 EOD;
     } else {
-        page_write($vars['refer'], $postdata);
+        
+        $current_page = $vars['refer'];
+
+        # 履歴を更新
+        update_nice_histroy($current_page);
+        
+        page_write($current_page, $postdata, PLUGIN_NICE_NOTIMESTAMP);
         $title = $_title_updated;
     }
 
@@ -66,7 +75,6 @@ EOD;
 function plugin_nice_convert()
 {
     global $script, $vars,  $digest;
-    global $_nice_plugin_choice, $_nice_plugin_nices;
     static $number = array();
 
     $page = isset($vars['page']) ? $vars['page'] : '';
@@ -109,5 +117,61 @@ EOD;
 EOD;
     }
     return $body;
+}
+
+function update_nice_histroy($current_page)
+{
+    global $date_format;
+
+    # 下記の流れで履歴を更新
+    # (1) 既に同じページ＆日付のものがあったらカウントアップ
+    # (2) 存在しない場合には現在日時で新たに追加
+    # (3) 古いものは消しておく
+    $current_date = get_date($date_format);
+    $period_date = get_date($date_format, strtotime('-' . PLUGIN_NICE_HISTORY_PERIOD_DAYS . ' day'));
+
+    pkwk_touch_file(PLUGIN_NICE_HISTORY_CACHE);
+    $fp = fopen(PLUGIN_NICE_HISTORY_CACHE, 'r+') or
+        die_message('Cannot open ' . PLUGIN_NICE_HISTORY_CACHE);
+    set_file_buffer($fp, 0);
+    flock($fp, LOCK_EX);
+
+    # 今までの履歴を読み込み
+    $histories = array();
+    $add_current = false;
+    foreach(file(PLUGIN_NICE_HISTORY_CACHE) as $line) {
+        list($date, $page, $cnt) = explode("\t", rtrim($line));
+
+        if (!isset($date) || !isset($page) || !isset($cnt) || strcmp($date, $current_date) > 0) {
+            # おかしなデータは除去
+            continue;
+        }
+
+        if (strcmp($date, $period_date) < 0) {
+            # 保存期間を超えて古いものは除去
+            continue;
+        }
+
+        if ($page == $current_page && $date == $current_date) {
+            $cnt++;
+            $add_current = true;
+        }
+
+        $histories[] = array('date' => $date, 'page' => $page, 'cnt' => $cnt);
+    }
+
+    if (!$add_current) {
+        $histories[] = array('date' => $current_date, 'page' => $current_page, 'cnt' => 1);
+        $add_current = true;
+    }
+
+    # 履歴を更新
+    ftruncate($fp, 0);
+    rewind($fp);
+    foreach ($histories as $history)
+        fputs($fp, $history['date'] . "\t" . $history['page'] . "\t" . $history['cnt'] . "\n");
+
+    flock($fp, LOCK_UN);
+    fclose($fp);
 }
 ?>
